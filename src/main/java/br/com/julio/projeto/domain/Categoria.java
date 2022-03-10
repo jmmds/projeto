de2702611b030e2a0081340dfaee377cdd62ee1d
Júlio Márcio <jmmds@outlook.com>
2022-03-10 10:19:43 -0300

    Banco de Dados H2 adicionado

--- src/main/java/br/com/julio/projeto/domain/Categoria.java
+++ src/main/java/br/com/julio/projeto/domain/Categoria.java
@@ -2,9 +2,14 @@ package br.com.julio.projeto.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
     private static final long serialVersionUID = 1L;
+    @Id
     private Integer id;
     private String nome;
 
