05c63f62ba3df96e8f070d367f09f72b225433f3
Júlio Márcio <jmmds@outlook.com>
2022-03-11 15:22:35 -0300

    Acrescentada a consulta na base de dados via API Rest

--- src/main/java/br/com/julio/projeto/ProjetoApplication.java
+++ src/main/java/br/com/julio/projeto/ProjetoApplication.java
@@ -3,8 +3,10 @@ package br.com.julio.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProjetoApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
