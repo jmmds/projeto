5beabeb766ba05451bd8a1b6d023228c4737f2a9
Júlio Márcio <jmmds@outlook.com>
2022-03-15 10:56:30 -0300

    Adicionadas as Classes - Endereço, Cliente e a Enum TipoCliente para controle de usuários

---
+++ src/main/java/br/com/julio/projeto/domain/Cliente.java
@@ -0,0 +1,123 @@
+package br.com.julio.projeto.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import br.com.julio.projeto.domain.enums.TipoCliente;
+
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private String cpfouCnpj;
+
+    private Integer tipo;
+
+    private List<Endereco> enderecos = new ArrayList<>();
+    
+    private Set<String> telefone = new HashSet<>();  // conjunto de Strings
+
+    public Cliente (){
+        
+    }
+
+    public Cliente(Integer id, String nome, String email, String cpfouCnpj, TipoCliente tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfouCnpj = cpfouCnpj;
+        this.tipo = tipo.getCod();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfouCnpj() {
+        return cpfouCnpj;
+    }
+
+    public void setCpfouCnpj(String cpfouCnpj) {
+        this.cpfouCnpj = cpfouCnpj;
+    }
+
+    public TipoCliente getTipo() {
+        return TipoCliente.toEnum(tipo);
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo.getCod();
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public Set<String> getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Set<String> telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+   
+
+    
+}
